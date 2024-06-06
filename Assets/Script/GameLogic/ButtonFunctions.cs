2c6071439c65ae1b7dbca17647267353e70d6c18
Tan Geng Cheng <tgc142322@gmail.com>
2024-06-07 01:55:33 +0800

    [Update] Implement the buttons, 2Dcollider
    - Done implemented buttons basic function and 2Dcollider
    - left: game duration text, room code text, green button(need to wait until server connection)
    - Next step: try connection with other players

---
+++ Assets/Script/GameLogic/ButtonFunctions.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+    public Button runButton;
+    public Button redButton;
+    public Button greenButton;
+
+    private bool canUseRunButton = true;
+
+    void Start()
+    {
+        // Assign button click listeners
+        runButton.onClick.AddListener(OnRunButtonClicked);
+        redButton.onClick.AddListener(OnRedButtonClicked);
+        greenButton.onClick.AddListener(OnGreenButtonClicked);
+    }
+
+    void OnRunButtonClicked()
+    {
+        if (canUseRunButton)
+        {
+            StartCoroutine(SpeedBoost());
+        }
+    }
+
+    void OnRedButtonClicked()
+    {
+        playerMovement.enabled = false; // Disable player movement
+    }
+
+    void OnGreenButtonClicked()
+    {
+        Debug.Log("Help: Use the run button to speed up, red button to stop, green button for help.");
+    }
+
+    IEnumerator SpeedBoost()
+    {
+        canUseRunButton = false;
+        playerMovement.speed += 5;
+        yield return new WaitForSeconds(5);
+        playerMovement.speed -= 5;
+        yield return new WaitForSeconds(10); // Wait additional 10 seconds to make it 15 seconds total
+        canUseRunButton = true;
+    }
+}
